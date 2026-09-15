9b1e0ba4f07f9dade2c4d373b50d57b64f1750bf
ericzhang000000@gmail.com <ericzhang000000@gmail.com>
2026-09-15 14:22:31 -0400

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './styles/tokens.css'
 import './styles/typography.css'
+import 'katex/dist/katex.min.css' // math rendering (quant write-ups)
+import 'highlight.js/styles/github.css' // code-block syntax highlighting
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
